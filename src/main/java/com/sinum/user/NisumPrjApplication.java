0a7e348b7dbedae2ba4eadce39e542c31e513a36
Yanina Di Prima <yanina.diprima@ayi.group>
2023-11-23 15:53:08 -0300

    Initial project

---
+++ src/main/java/com/sinum/user/NisumPrjApplication.java
@@ -0,0 +1,13 @@
+package com.sinum.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisumPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NisumPrjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinum/user/NisumPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinum.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NisumPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
